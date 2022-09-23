933a036c97545367c5f6258c34af774507fe7bfe
abhilash28abhi <abhilash28.abhi@gmail.com>
2022-09-24 00:24:04 +0530

    added junit tests for controller

--- src/test/java/com/contactlist/app/contactlist/controller/ContactListControllerTest.java
+++ src/test/java/com/contactlist/app/contactlist/controller/ContactListControllerTest.java
@@ -1,6 +1,7 @@
 package com.contactlist.app.contactlist.controller;
 
 import com.contactlist.app.contactlist.api.ContactListResponse;
+import com.contactlist.app.contactlist.exception.DataViolationException;
 import com.contactlist.app.contactlist.model.Contact;
 import com.contactlist.app.contactlist.service.impl.ContactListManager;
 import org.junit.Test;
@@ -15,10 +16,10 @@ import org.springframework.http.ResponseEntity;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ContactListControllerTest {
@@ -32,14 +33,51 @@ public class ContactListControllerTest {
     @Test
     public void getContactDetails_success () throws Exception {
         List<Contact> contactList = new ArrayList<>();
-        Contact contact = new Contact("Spring Boot @WebMvcTest", "Description");
+        Contact contact = new Contact("James David", "Url1");
         contactList.add(contact);
         ContactListResponse response = new ContactListResponse();
         response.setContacts(contactList);
 
-        doReturn(response).when(manager).getContactDetails(any());
-        when(manager.getContactDetails(PageRequest.of(0,5))).thenReturn(response);
+        when(manager.getContactDetails(any())).thenReturn(response);
         ResponseEntity<?> resp = contactListController.getContactDetails("",0, 2);
         assertEquals(HttpStatus.OK, resp.getStatusCode());
+        ContactListResponse respBody = (ContactListResponse) resp.getBody();
+        assertEquals("Url1", respBody.getContacts().get(0).getUrl());
+    }
+
+    @Test
+    public void getContactDetails_empty_response () throws Exception {
+        List<Contact> contactList = new ArrayList<>();
+        ContactListResponse response = new ContactListResponse();
+        response.setContacts(contactList);
+
+        lenient().when(manager.getContactDetails(any())).thenReturn(response);
+        ResponseEntity<?> resp = contactListController.getContactDetails("David",0, 5);
+        assertEquals(HttpStatus.NO_CONTENT, resp.getStatusCode());
+    }
+
+    @Test
+    public void getContactDetailsByName_success () throws Exception {
+        List<Contact> contactList = new ArrayList<>();
+        Contact contact = new Contact("James David", "Url1");
+        contactList.add(contact);
+        ContactListResponse response = new ContactListResponse();
+        response.setContacts(contactList);
+
+        when(manager.getContactDetailsByName(anyString(), any())).thenReturn(response);
+        ResponseEntity<?> resp = contactListController.getContactDetails("David",0, 5);
+        assertEquals(HttpStatus.OK, resp.getStatusCode());
+        ContactListResponse respBody = (ContactListResponse) resp.getBody();
+        assertEquals("James David", respBody.getContacts().get(0).getName());
+    }
+
+    @Test
+    public void getContactDetailsByName_invalid_page () throws Exception {
+        lenient().doThrow(new DataViolationException()).when(manager).getContactDetailsByName(anyString(), any());
+        try {
+            contactListController.getContactDetails("David",-2, 5);
+        } catch (Exception e) {
+            assertTrue(e instanceof DataViolationException);
+        }
     }
 }
